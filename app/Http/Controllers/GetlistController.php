93badce294690fb661567a2a56947bb01f45cc74
Bonaventure Umolu <49821157+devburna@users.noreply.github.com>
2021-11-07 16:42:18 +0100

    update

---
+++ app/Http/Controllers/GetlistController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Getlist;
+use Illuminate\Http\Request;
+
+class GetlistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Getlist  $getlist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Getlist $getlist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Getlist  $getlist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Getlist $getlist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Getlist  $getlist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Getlist $getlist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Getlist  $getlist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Getlist $getlist)
+    {
+        //
+    }
+}
