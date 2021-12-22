8c84bafd6abb364cc24d2636c3465ed944e680d6
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2021-12-22 18:06:40 +0100

    Update VirtualCardController.php

--- app/Http/Controllers/VirtualCardController.php
+++ app/Http/Controllers/VirtualCardController.php
@@ -5,35 +5,38 @@ namespace App\Http\Controllers;
 use App\Http\Requests\VirtualCardRequest;
 use App\Models\VirtualCard;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class VirtualCardController extends Controller
 {
     public function create(VirtualCardRequest $request)
     {
 
-        $response =  (new GladeController())->createVirtualCard($request->user()->name, $request->amount);
+        return DB::transaction(function ($request) {
+            $response =  (new GladeController())->createVirtualCard($request->user()->name, $request->amount);
 
-        if (!$response) {
+            if (!$response) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Error creating card.'
+                ], 422);
+            }
+
+            VirtualCard::create([
+                'user_id' => $request->user()->id,
+                'reference' => $response['card_data']['reference'],
+                'provider' => $response['provider'],
+            ]);
+
+            $request->user()->wallet->update([
+                'balance' => $request->user()->wallet->balance - ($request->amount + 2),
+            ]);
+
             return response()->json([
-                'status' => false,
-                'message' => 'Error creating card.'
-            ], 422);
-        }
-
-        VirtualCard::create([
-            'user_id' => $request->user()->id,
-            'reference' => $response['card_data']['reference'],
-            'provider' => $response['provider'],
-        ]);
-
-        $request->user()->wallet->update([
-            'balance' => $request->user()->wallet->balance - ($request->amount + 2),
-        ]);
-
-        return response()->json([
-            'status' => true,
-            'message' => 'Card created.'
-        ], 201);
+                'status' => true,
+                'message' => 'Card created.'
+            ], 201);
+        });
     }
 
     public function store(VirtualCardRequest $request)
