cac8da899b116a33782c8f7301cd510067e00041
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2022-08-21 21:37:44 +0100

    chore:

--- routes/api.php
+++ routes/api.php
@@ -125,7 +125,7 @@ Route::prefix('v1')->group(function () {
         });
 
         # redeem gift
-        Route::prefix('redeem-gift')->middleware(['abilities:authenticate,redeem-gift-card'])->group(function () {
+        Route::prefix('redeem-gift')->middleware(['ability:redeem-gift-card'])->group(function () {
 
             # preview
             Route::get('', [\App\Http\Controllers\GiftCardController::class, 'preview']);
